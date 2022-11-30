fb7df9bf98d662e0e7b1fb7d0efdefb9649c6468
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2022-11-30 18:36:48 +0300

    Start project

---
+++ src/main/java/kz/zhelezyaka/springboot3experiment/SpringBoot3ExperimentApplication.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.springboot3experiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3ExperimentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot3ExperimentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/zhelezyaka/springboot3experiment/SpringBoot3ExperimentApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.springboot3experiment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3ExperimentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
