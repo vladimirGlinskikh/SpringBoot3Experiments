075050f3ceb852f28cab1262a14edc3dfad82533
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2022-11-30 20:29:37 +0300

    Java Objects to JSON Objects

--- src/main/java/kz/zhelezyaka/springboot3experiment/controller/Controllers.java
+++ src/main/java/kz/zhelezyaka/springboot3experiment/controller/Controllers.java
@@ -4,6 +4,8 @@ import kz.zhelezyaka.springboot3experiment.model.Greet.GreetResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 import static kz.zhelezyaka.springboot3experiment.model.Persons.*;
 
 @RestController
@@ -11,11 +13,14 @@ public class Controllers {
 
     @GetMapping("/greet")
     public GreetResponse greet() {
-        return new GreetResponse("Hey guru Spring!");
+        return new GreetResponse("Hey guru Spring!",
+                List.of("Java", "SQL", "JavaScript"),
+                List.of(new Person("Vladimir", "Glinskikh"),
+                        new Person("Svetlana", "Marchenko")));
     }
 
     @GetMapping("/person")
     public Person person() {
-        return new Person("Vladimir", "Glinskikh");
+        return new Person("Nikolay", "Romanov");
     }
 }
--- src/main/java/kz/zhelezyaka/springboot3experiment/model/Greet.java
+++ src/main/java/kz/zhelezyaka/springboot3experiment/model/Greet.java
@@ -1,6 +1,10 @@
 package kz.zhelezyaka.springboot3experiment.model;
 
+import java.util.List;
+
 public class Greet {
-    public record GreetResponse(String greet) {
+    public record GreetResponse(String greet,
+                                List<String> programmingLanguages,
+                                List<Persons.Person> person) {
     }
 }
